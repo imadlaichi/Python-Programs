08ec79cd80e1cbea2906974eb0d20a24c2570ef7
Imad Laichi <141133660+imadlaichi@users.noreply.github.com>
2024-04-11 17:26:42 +0200

    Create operaciones-basicas.py

---
+++ Spanish/operaciones-basicas.py
@@ -0,0 +1,10 @@
+n1 = 0
+n2 = 0
+while n1 == 0:
+    n1 = int(input("Introduce el primer número: "))
+while n2 == 0:
+    n2 = int(input("Introduce el segundo número: "))
+print(n1, "+", n2, "=", n1 + n2)
+print(n1, "-", n2, "=", n1 - n2)
+print(n1, "x", n2, "=", n1 * n2)
+print(n1, "/", n2, "=", n1 / n2)
